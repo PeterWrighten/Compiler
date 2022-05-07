a84ccffe8e74a780fd55dff2f5a7a8e059b7c374
PeterWrighten <peterwrighten@gmail.com>
2022-05-07 20:45:28 +0900

    Update: Rust

--- peter_compiler/src/main.rs
+++ peter_compiler/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use lalrpop_util::lalrpop_mod;
+use std::env::args;
+use std::fs::read_to_string;
+use std::io::Result;
+
+lalrpop_mod!(peter);
+fn main() -> Result<()> {
+    let mut args = args();
+    args.next();
+    let mode = args.next().unwrap();
+    let input = args.next().unwrap();
+    args.next();
+    let output = args.next().unwrap();
+
+    let input = read_to_string(input)?;
+    let ast = peter::CompUnitParser::new().parse(&input).unwrap();
+
+    println!("{}", ast);
+    Ok(())
 }
