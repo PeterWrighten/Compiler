9cb753098ed376e805277a06d4af01580663c72f
PeterWrighten <peterwrighten@gmail.com>
2022-08-11 23:59:04 +0900

    update: ast

--- PeterCompiler/src/ast/ast.hpp
+++ PeterCompiler/src/ast/ast.hpp
@@ -1,4 +1,8 @@
 class NegExpr: public Expr {
     public: 
         NegExpr(Expr *e, Location *l);
-}
+        virtual void accept(Visitor*);
+        virtual void dumpTo(std::ostream &);
+    public:
+        Expr *e;
+};
